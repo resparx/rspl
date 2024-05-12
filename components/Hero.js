1aa4f278e144dc1a3b2db97d44e0801a2eff68ef
rsi-ramprash <ramprasanth.m@ramsoft.com>
2024-05-12 23:47:05 +0530

    fix: responsiveness

--- components/Hero.js
+++ components/Hero.js
@@ -9,68 +9,81 @@ const Hero = () => {
   const data = [
     {
       title: "Clinical Decision Support",
-      content: "Creating AI-driven tools that analyze patient data, medical histories, and clinical guidelines to provide healthcare professionals with personalized treatment recommendations, drug interactions alerts, and risk assessments",
+      content:
+        "Creating AI-driven tools that analyze patient data, medical histories, and clinical guidelines to provide healthcare professionals with personalized treatment recommendations, drug interactions alerts, and risk assessments",
       number: "01",
       wrapperClasses: "bg-yellow-100",
       theme: "light",
-      icon: <Brain className="fill-slate-800" height={300} />
+      icon: <Brain className="fill-slate-800 h-[100px] lg:h-[300px]" />,
     },
     {
       title: "Remote Patient Monitoring",
-      content: "Designing AI-powered devices and software for remote patient monitoring, allowing healthcare providers to track patients' vital signs, symptoms, and adherence to treatment plans in real-time, leading to proactive intervention and improved chronic disease management",
+      content:
+        "Designing AI-powered devices and software for remote patient monitoring, allowing healthcare providers to track patients' vital signs, symptoms, and adherence to treatment plans in real-time, leading to proactive intervention and improved chronic disease management",
       wrapperClasses: "bg-cyan-200",
       number: "02",
       theme: "light",
-      icon: <ThermoMeter className="fill-slate-800" height={300} />
+      icon: <ThermoMeter className="fill-slate-800  h-[100px] lg:h-[300px]" />,
     },
     {
       title: "Drug Discovery and Development",
-      content: "Utilizing ML algorithms to analyze vast amounts of biological data, identify potential drug targets, predict drug efficacy, and accelerate the drug discovery process, ultimately leading to the development of more effective and targeted therapies",
+      content:
+        "Utilizing ML algorithms to analyze vast amounts of biological data, identify potential drug targets, predict drug efficacy, and accelerate the drug discovery process, ultimately leading to the development of more effective and targeted therapies",
       wrapperClasses: "bg-white",
       theme: "light",
       number: "03",
-      icon: <Pills className="fill-slate-800" height={300} />
+      icon: <Pills className="fill-slate-800  h-[100px] lg:h-[300px]" />,
     },
     {
       title: "Healthcare Operations Optimization",
-      content: "Implementing AI-driven solutions for healthcare operations, such as predictive analytics for resource allocation, supply chain optimization, staff scheduling, and workflow automation, to improve efficiency and reduce costs",
+      content:
+        "Implementing AI-driven solutions for healthcare operations, such as predictive analytics for resource allocation, supply chain optimization, staff scheduling, and workflow automation, to improve efficiency and reduce costs",
       wrapperClasses: "bg-rose-300",
       theme: "light",
       number: "04",
-      icon: <BriedCaseMed className="fill-slate-800"  height={300} />,
-      
+      icon: <BriedCaseMed className="fill-slate-800  h-[100px] lg:h-[300px]" />,
     },
     {
       title: "Natural Language Processing (NLP)",
-      content: "Developing NLP algorithms to extract and analyze unstructured medical data from electronic health records (EHRs), clinical notes, and research literature, enabling insights into patient outcomes, disease trends, and treatment effectiveness",
+      content:
+        "Developing NLP algorithms to extract and analyze unstructured medical data from electronic health records (EHRs), clinical notes, and research literature, enabling insights into patient outcomes, disease trends, and treatment effectiveness",
       wrapperClasses: "bg-orange-300",
       theme: "light",
-      icon: <Chip className="fill-slate-800" height={300} />,
-      number: "05"
+      icon: <Chip className="fill-slate-800 h-[100px] lg:h-[300px]" />,
+      number: "05",
     },
     {
       title: "Telemedicine Platforms",
-      content: "Building AI-powered telemedicine platforms that offer virtual consultations, symptom triage, medical chatbots, and remote monitoring capabilities, expanding access to healthcare services and improving patient engagement",
+      content:
+        "Building AI-powered telemedicine platforms that offer virtual consultations, symptom triage, medical chatbots, and remote monitoring capabilities, expanding access to healthcare services and improving patient engagement",
       number: "06",
       wrapperClasses: "bg-slate-700",
-      icon: <CommentMedical  className="fill-white"  height={300} />,
-      theme: "dark"
-    }
+      icon: <CommentMedical className="fill-white h-[100px] lg:h-[300px]" />,
+      theme: "dark",
+    },
   ];
   return (
     <section className=" flex gap-12 justify-between items-center flex-col lg:flex-row py-0">
       <div className="flex flex-col">
-        <div className="pb-[3%] pt-[10%] pl-[20%] pr-[10%]">
-          <p className="font-semibold text-3xl text-slate-800">
-          RSPL Health specializing in AI (Artificial Intelligence) and ML (Machine Learning) solutions typically offers innovative products and services that leverage these technologies to enhance healthcare delivery and improve patient outcomes. Here&apos;s a description
+        <div className="pb-[3%] pt-[10%] pl-[5%] lg:pl-[20%] pr-[10%]">
+          <p className="font-semibold text-xl lg:text-3xl text-slate-800">
+            RSPL Health:
+            <br/>
+            <br/>
+            Specializing in AI (Artificial Intelligence) and ML
+            (Machine Learning) solutions typically offers innovative products
+            and services that leverage these technologies to enhance healthcare
+            delivery and improve patient outcomes.
           </p>
-          <p className="text-slate-800 text-xl mt-20">
-          AI and ML-Powered Medical Solutions
+          <br/>
+          <p>Here's a description</p>
+          <p className="text-slate-800 text:lg lg:text-xl mt-20">
+            AI and ML-Powered Medical Solutions
           </p>
         </div>
-      {data.map((item, index) => (
-        <Section key={`section-${index}`} {...item} />
-      ))}
+        {data.map((item, index) => (
+          <Section key={`section-${index}`} {...item} />
+        ))}
       </div>
     </section>
   );
--- components/Section.js
+++ components/Section.js
@@ -6,19 +6,19 @@ const Section = ({ number = "01", sectionTitle ="Services", theme, title, conten
     const fontColor = theme === "light" ? "text-slate-800" : "white"
     const bgColor = theme === "light" ? "bg-slate-200" : "bg-slate-800" 
   return (
-    <section className={`flex items-center gap-12 h-[70vh] pl-[20%] ${wrapperClasses}`}>
-      <div className="flex flex-col gap-6 items-center">
+    <section className={`w-screen flex items-center gap-6 lg:gap-12 h-[88vh] lg:h-[70vh] pl-[5%] lg:pl-[20%] ${wrapperClasses}`}>
+      <div className="flex flex-col gap-3 lg:gap-6 items-center">
         <div className={`rounded-full p-2 ${bgColor}`}>
-          <p className={`${fontColor} text-xl font-semibold`}>{number}</p>
+          <p className={`${fontColor} text-sm lg:text-xl font-semibold`}>{number}</p>
         </div>
-        <p className={`${fontColor} [writing-mode:vertical-lr] font-semibold text-3xl tracking-widest`}>{sectionTitle}</p>
+        <p className={`${fontColor} [writing-mode:vertical-lr] font-semibold text-lg lg:text-3xl tracking-widest`}>{sectionTitle}</p>
       </div>
-      <div className="flex gap-20 items-center">
+      <div className="flex flex-col-reverse lg:flex-row gap-8 lg:gap-20 items-center">
       <div className="flex flex-col">
-            <p className={`${fontColor} text-7xl font-semibold w-[80%] mb-12`}>{title}</p>
-            <p className={`${fontColor} w-[80%] text-xl font-light`}>{content}</p>
+            <p className={`${fontColor} text-2xl lg:text-7xl font-semibold w-[80%] mb-4 lg:mb-12`}>{title}</p>
+            <p className={`${fontColor} w-[80%] text:xs lg:text-xl font-light`}>{content}</p>
       </div>
-      <div className="mr-20">
+      <div className="w-full lg:mr-20">
         {icon}
       </div>
       </div>
